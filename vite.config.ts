5843926f3a5c7dc4e6b953437bda14bf31873940
Ntsiyeni Ganyane <rmnganyane@gmail.com>
2026-09-08 14:29:09 +0200

    Commit Changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/', // Required for root domains like Render
   plugins: [react()],
   resolve: {
     alias: {
