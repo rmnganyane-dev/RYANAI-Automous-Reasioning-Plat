e6951645c258e398150c8155705f01b4410a7a8e
Ntsiyeni Ganyane <rmnganyane@gmail.com>
2026-09-09 16:48:29 +0200

    deploy: push latest updates to main

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,15 @@ import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
+  server: {
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      }
+    }
   },
   build: {
     chunkSizeWarningLimit: 1000,
