d027b0f965e787f870f171020421b61fd250bd56
Ntsiyeni Ganyane <rmnganyane@gmail.com>
2026-09-09 17:19:14 +0200

    ship: production build update

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
   server: {
     port: 5173,
     proxy: {
